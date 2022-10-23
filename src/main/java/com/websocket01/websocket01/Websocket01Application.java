58f3f76bf764974c4a6bc56564af18c1d3f82a23
Samuele Catalano <csamuele73@mail.com>
2022-10-23 18:12:00 +0200

    first commit

---
+++ src/main/java/com/websocket01/websocket01/Websocket01Application.java
@@ -0,0 +1,13 @@
+package com.websocket01.websocket01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Websocket01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Websocket01Application.class, args);
+	}
+
+}
